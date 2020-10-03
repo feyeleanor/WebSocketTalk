889f0e95083fc529f12f968f7ff6e2b8f20fc6d8
Eleanor McHugh <e.mchugh@inidsol.uk>
2020-10-03 17:40:35 +0100

    example using an environment variable to override default PORT allocation

---
+++ 03.go
@@ -0,0 +1,27 @@
+package main
+import . "fmt"
+import "net/http"
+import "os"
+
+const MESSAGE = "hello world"
+var ADDRESS string
+
+func init() {
+	if p := os.Getenv("PORT"); len(p) == 0 {
+		ADDRESS = ":3000"
+	} else {
+		ADDRESS = ":" + p
+	}
+}
+
+func main() {
+  http.HandleFunc("/", billboard)
+  if e := http.ListenAndServe(ADDRESS, nil); e != nil {
+    Println(e)
+  }
+}
+
+func billboard(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/plain")
+  Fprintf(w, MESSAGE)
+}
