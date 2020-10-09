b8d945ce240f88f87f52fe0cf8ecfafc76a3cc3a
Eleanor McHugh <e.mchugh@inidsol.uk>
2020-10-09 16:13:10 +0100

    HTTP method handling

---
+++ 12.go
@@ -0,0 +1,86 @@
+package main
+import "fmt"
+import "html/template"
+import "net/http"
+import "os"
+
+const LAUNCH_FAILED = 1
+const FILE_READ = 2
+const BAD_TEMPLATE = 3
+
+const VERSION = 12
+
+var ADDRESS string
+
+func init() {
+	if p := os.Getenv("PORT"); len(p) == 0 {
+		ADDRESS = ":3000"
+	} else {
+		ADDRESS = ":" + p
+	}
+}
+
+type Handler func(http.ResponseWriter, *http.Request)
+type CallBridge map[template.JS] Handler
+type PageConfiguration struct {
+	Version int
+	CallBridge
+}
+
+func main() {
+	p :=  PageConfiguration{ VERSION, CallBridge {
+			"A": AJAX_handler("A"),
+			"B": AJAX_handler("B"),
+			"C": AJAX_handler("C"),
+		},
+	}
+
+	html := LoadTemplate(Filename(VERSION, "html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		Abort(BAD_TEMPLATE, html.Execute(w, p))
+	})
+
+	js_file := Filename(VERSION, "js")
+	js := LoadTemplate(js_file)
+	http.HandleFunc("/" + js_file, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		Abort(BAD_TEMPLATE, js.Execute(w, p))
+	})
+
+ 	for c, f := range p.CallBridge {
+		http.HandleFunc(fmt.Sprint("/", c), f)
+	}
+	Abort(LAUNCH_FAILED, http.ListenAndServe(ADDRESS, nil))
+}
+
+func Abort(n int, e error) {
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(n)
+	}
+}
+
+func AJAX_handler(c string) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "GET (%v) %v", c, r.URL)
+		case "POST":
+			r.ParseForm()
+			fmt.Fprintf(w, "POST (%v) %v {%v}", c, r.URL, r.Form)
+		}
+	}
+}
+
+func LoadTemplate(s string) (r *template.Template) {
+	var e error
+	r, e = template.ParseFiles(s)
+	Abort(FILE_READ, e)
+	return
+}
+
+func Filename(n int, ext string) string {
+	return fmt.Sprintf("%v.%v", n, ext)
+}
---
+++ 12.js
@@ -0,0 +1,26 @@
+function print(m) {
+	document.getElementById("event_log").innerHTML += "<div>" + m + "</div>";
+}
+
+{{range $c, $v := .CallBridge}}
+	function {{$c}}(method) {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				console.log(this.responseText);
+				print(this.responseText);
+			}
+		};
+		switch(method) {
+		case "GET":
+			xhttp.open("GET", "{{$c}}", true);
+			xhttp.send();
+			break;
+		case "POST":
+			xhttp.open("POST", "{{$c}}", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send("c={{$c}}&{{$v}}=v");
+			break;
+		}
+	}
+{{end}}
