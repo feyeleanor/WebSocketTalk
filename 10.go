559ecb62ef35522fae64aebb40780f27d184f4a5
Eleanor McHugh <e.mchugh@inidsol.uk>
2020-10-09 12:16:27 +0100

    factored out JS code

--- 10.go
+++ 10.go
@@ -8,8 +8,9 @@ const LAUNCH_FAILED = 1
 const FILE_READ = 2
 const BAD_TEMPLATE = 3
 
+const RESOURCES = "10"
+
 var ADDRESS string
-var AJAX_COMMANDS map[string] func(http.ResponseWriter, *http.Request)
 
 func init() {
 	if p := os.Getenv("PORT"); len(p) == 0 {
@@ -17,27 +18,38 @@ func init() {
 	} else {
 		ADDRESS = ":" + p
 	}
-
-	AJAX_COMMANDS = map[string] func(http.ResponseWriter, *http.Request) {
-		"A": make_AJAX_command("A"),
-		"B": make_AJAX_command("B"),
-		"C": make_AJAX_command("C"),
-	}
 }
 
-type PageContent struct {
+type PageConfiguration struct {
 	Commands map[string] func(http.ResponseWriter, *http.Request)
 }
 
 func main() {
-	html, e := template.ParseFiles("10.html")
-	halt_on_error(FILE_READ, e)
+	html, e1 := template.ParseFiles(fmt.Sprintf("%v.html", RESOURCES))
+	halt_on_error(FILE_READ, e1)
+
+	js_temp, e2 := template.ParseFiles(fmt.Sprintf("%v.js", RESOURCES))
+	halt_on_error(FILE_READ, e2)
+
+	p :=  PageConfiguration{
+		map[string] func(http.ResponseWriter, *http.Request) {
+			"A": AJAX_handler("A"),
+			"B": AJAX_handler("B"),
+			"C": AJAX_handler("C"),
+		},
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		halt_on_error(BAD_TEMPLATE, html.Execute(w, PageContent{ AJAX_COMMANDS }))
+		halt_on_error(BAD_TEMPLATE, html.Execute(w, p))
 	})
-	for c, f := range AJAX_COMMANDS {
+
+	http.HandleFunc("/10.js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/javascript")
+		halt_on_error(BAD_TEMPLATE, js_temp.Execute(w, p))
+	})
+
+ 	for c, f := range p.Commands {
 		http.HandleFunc(fmt.Sprint("/", c), f)
 	}
 	halt_on_error(LAUNCH_FAILED, http.ListenAndServe(ADDRESS, nil))
@@ -50,9 +62,8 @@ func halt_on_error(n int, e error) {
 	}
 }
 
-func make_AJAX_command(c string) func(http.ResponseWriter, *http.Request) {
+func AJAX_handler(c string) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-fmt.Println("calling", c)
 		w.Header().Set("Content-Type", "text/plain")
 		fmt.Fprint(w, c)
 	}
