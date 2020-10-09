e05414e03d203816b575ee0fc2aef687a5342cd0
Eleanor McHugh <e.mchugh@inidsol.uk>
2020-10-09 16:12:47 +0100

    name changes, reorganisation

--- 10.js
+++ 10.js
@@ -1,10 +1,14 @@
+function print(m) {
+	document.getElementById("event_log").innerHTML += "<div>" + m + "</div>";
+}
+
 {{range $c, $v := .Commands}}
 	function {{$c}}() {
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				console.log(this.responseText);
-				document.getElementById("event_log").innerHTML += "<span>" + this.responseText + "</span>";
+				print(this.responseText);
 			}
 		};
 		xhttp.open("GET", "{{$c}}", true);
