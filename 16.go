70b1c73837afa8f8c17e7102327ec96c482afaa0
Eleanor McHugh <e.mchugh@inidsol.uk>
2020-10-12 22:04:07 +0100

    basic websocket example to provide a realtime data feed

---
+++ 16.go
@@ -0,0 +1,179 @@
+package main
+import "fmt"
+import "golang.org/x/net/websocket"
+import H "html/template"
+import "io/ioutil"
+import "net/http"
+import "os"
+import "strconv"
+import "strings"
+import T "text/template"
+import "time"
+
+const LAUNCH_FAILED = 1
+const FILE_READ = 2
+const BAD_TEMPLATE = 3
+
+const TIME_FORMAT = "Mon Jan 2 15:04:05 MST 2006"
+const PUBLIC_ID = "public"
+
+var VERSION, ADDRESS string
+
+func init() {
+	s := strings.Split(os.Args[0], "/")
+	VERSION = s[len(s) - 1]
+
+	if p := os.Getenv("PORT"); len(p) == 0 {
+		ADDRESS = ":3000"
+	} else {
+		ADDRESS = ":" + p
+	}
+}
+
+type Message struct {
+	TimeStamp, Author, Content string
+}
+type PigeonHole []Message
+type PigeonHoles map[string] PigeonHole
+type PageConfiguration struct {
+	Version string
+	Clients int
+	PigeonHoles
+}
+
+func main() {
+	var e error
+	var html *H.Template
+	var js *T.Template
+
+	p :=  &PageConfiguration {
+		Version: VERSION,
+		PigeonHoles: make(PigeonHoles),
+	}
+	events := make(chan string, 4096)
+
+	html, e = H.ParseFiles(VERSION + ".html")
+	Abort(FILE_READ, e)
+
+	js_file := VERSION + ".js"
+	js, e = T.ParseFiles(js_file)
+	Abort(FILE_READ, e)
+
+	http.HandleFunc("/", Monitor(events, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		Abort(BAD_TEMPLATE, html.Execute(w, p))
+		p.Clients += 1
+	}))
+
+	http.HandleFunc("/message", Monitor(events, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/message")
+		w.Header().Set("Content-Type", "text/plain")
+		r.ParseForm()
+		switch r.Method {
+		case "GET":
+			q := Feed("r", r)
+			ph := p.PigeonHoles[q]
+			if i := MessageIndex(r); i < len(ph) {
+				m := ph[i]
+				fmt.Fprintf(w, "%v\n%v\n%v", m.Author, m.TimeStamp, m.Content)
+			} else {
+				http.NotFound(w, r)
+			}
+		case "POST":
+			q := Feed("r", r)
+			p.PigeonHoles[q] = append(p.PigeonHoles[q], Message {
+				TimeStamp: time.Now().Format(TIME_FORMAT),
+				Author: r.PostForm.Get("a"),
+				Content: r.PostForm.Get("m"),
+			})
+		}
+	}))
+
+	http.HandleFunc("/messages", Monitor(events, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/messages")
+		w.Header().Set("Content-Type", "text/plain")
+		r.ParseForm()
+		fmt.Fprint(w, len(p.PigeonHoles[Feed("a", r)]))
+	}))
+
+	http.HandleFunc("/" + js_file, Monitor(events, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/" + js_file)
+		w.Header().Set("Content-Type", "application/javascript")
+		Abort(BAD_TEMPLATE, js.Execute(w, p))
+	}))
+
+	var monitor_feeds []*websocket.Conn
+	var events_broadcast int
+	go func() {
+		for {
+			select {
+			case m := <- events:
+				events_broadcast += 1
+				for _, ws := range monitor_feeds {
+					fmt.Fprintf(ws, "%v\n%v", events_broadcast, m)
+					
+				}
+			default:
+				time.Sleep(1 * time.Millisecond)
+			}
+		}
+	}()
+
+	http.Handle("/monitor", websocket.Handler(func(ws *websocket.Conn) {
+		defer func() {
+			fmt.Println("closing websocket")
+			if e := ws.Close(); e != nil {
+				events <- e.Error()
+			}
+		}()
+		monitor_feeds = append(monitor_feeds, ws)
+		ioutil.ReadAll(ws)
+	}))
+
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("/events")
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprint(w, events_broadcast)
+	})
+
+	Abort(LAUNCH_FAILED, http.ListenAndServe(ADDRESS, nil))
+}
+
+func Abort(n int, e error) {
+	if e != nil {
+		fmt.Println(e)
+		os.Exit(n)
+	}
+}
+
+func ParseIndex(s string) (int, error) {
+	u, e := strconv.ParseUint(s, 10, 64)
+	return int(u), e
+}
+
+func MessageIndex(r *http.Request) (i int) {
+	if len(r.Form["i"]) != 0 {
+		if n, e := ParseIndex(r.Form["i"][0]); e == nil {
+			i = n
+		}
+	}
+	return
+}
+
+func Feed(n string, r *http.Request) (s string) {
+	if id := r.Form[strings.ToLower(n)]; len(id) == 0 {
+		s = PUBLIC_ID
+	} else if s = id[0]; len(s) == 0 {
+		s = PUBLIC_ID
+	}
+	return
+}
+
+type WebHandler func(http.ResponseWriter, *http.Request)
+
+func Monitor(c chan string, f WebHandler) WebHandler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c <- r.URL.String()
+		f(w, r)
+	}
+}
---
+++ 16.js
@@ -0,0 +1,126 @@
+function read_var(v) {
+	return document.getElementById(v).value;
+}
+
+function print(e, m) {
+	document.getElementById(e).innerHTML += `<div>${m}</div>`;
+}
+
+function update(e, m) {
+	document.getElementById(e).innerHTML = m;
+}
+
+function format_message(t) {
+	var m = t.split("\n");
+	var author = m.shift();
+	var timestamp = m.shift();
+	var message = m.shift();
+	return `<h3>${author}</h3><div>${timestamp}</div><div>${message}</div>`;
+}
+
+function move_message(e, o, d) {
+	document.getElementById(`${e}_${d}`).innerHTML = document.getElementById(`${e}_${o}`).innerHTML;
+}
+
+function update_message_buffer(e, c, m) {
+	move_message(e, 1, 2);
+	move_message(e, 0, 1);
+	update(`${e}_0`, m);
+	update(`${e}_count`, c);
+}
+
+function ajax_setup(f) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			f(xhttp);
+		}
+	};
+	return xhttp;	
+}
+
+function ajax_get(url, response_handler) {
+	var xhttp = ajax_setup(x => response_handler(x.responseText));
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}
+
+function ajax_post(xhttp, url, params) {
+	xhttp.open("POST", url, true);
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send(params);
+}
+
+var public_seen = 0;
+var public_total = 0;
+var private_seen = 0;
+var private_total = 0;
+
+function post_comment() {
+	var xhttp = ajax_setup(x => {
+		var f = document.forms["addMessage"];
+		f.recipient.value = "";
+		f.message.value = "";
+	});
+
+	var recipient = read_var('recipient');
+	ajax_post(xhttp, "message", `a=${read_var('client_id')}&m=${read_var('message')}&r=${recipient}`);
+}
+
+function server_link(interval, f) {
+	setInterval(f, interval)
+}
+
+server_link(1000, () => {
+	if (public_seen < public_total) {
+		ajax_get(`/message?r=public&i=${public_seen}`, response => {
+			public_seen++;
+			update_message_buffer("public_list", public_total, format_message(response));
+		})
+	}
+});
+
+server_link(1000, () => {
+	if (private_seen < private_total) {
+		ajax_get(`/message?r=${read_var('client_id')}&i=${private_seen}`, response => {
+			private_seen++;
+			update_message_buffer("private_list", private_total, format_message(response));
+		})
+	}
+});
+
+server_link(500, () =>
+	ajax_get("/messages?r=public", r => public_total = r))
+
+server_link(500, () =>
+	ajax_get(`/messages?r=private&a=${read_var('client_id')}`, r => private_total = r))
+
+function server_socket(url, onMessage) {
+	if ('WebSocket' in window) {
+		var socket = new WebSocket(url);
+		socket.onopen = function(e) {
+			console.log(`opening socket: ${url}`);
+		};
+		socket.onclose = function(e) {
+			console.log(`closing socket: ${url}`);
+		};
+		socket.onerror = function(error) {
+			console.log(`error for ${url}: ${error.message}`);
+		};
+		socket.onmessage = onMessage;
+		return socket;
+	} else {
+		//	use AJAX polling as an alternative?
+		return null;
+	}
+}
+
+var monitor_feed;
+
+window.onload = function() {
+	update("id_banner", `contact ID: ${read_var('client_id')}`);
+	monitor_feed = server_socket("ws://localhost:3000/monitor", m => {
+		var d = m.data.split("\n");
+		update_message_buffer("event_list", d[0], d[1])
+	})
+}
